388cbb9a8a3f3cc8ecb9845e39d4a2af955209b4
wonpark1 <2023111927@dgu.ac.kr>
2026-01-04 12:14:06 +0900

    chore: biome lint errors 수정

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { App } from '@/app/App'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { App } from "@/app/App";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const rootElement = document.getElementById("root");
+
+if (!rootElement) {
+	throw new Error("Root element not found");
+}
+
+createRoot(rootElement).render(
+	<StrictMode>
+		<App />
+	</StrictMode>,
+);
--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import path from 'path'
+import path from "node:path";
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-      '@app': path.resolve(__dirname, './src/app'),
-      '@pages': path.resolve(__dirname, './src/pages'),
-      '@widgets': path.resolve(__dirname, './src/widgets'),
-      '@shared': path.resolve(__dirname, './src/shared'),
-      '@apis': path.resolve(__dirname, './src/shared/apis'),
-      '@svg': path.resolve(__dirname, './src/shared/assets/svg'),
-      '@img': path.resolve(__dirname, './src/shared/assets/img'),
-      '@styles': path.resolve(__dirname, './src/shared/styles'),
-      '@libs': path.resolve(__dirname, './src/shared/libs'),
-      '@configs': path.resolve(__dirname, './src/shared/configs'),
-      '@ui': path.resolve(__dirname, './src/shared/ui'),
-    },
-  },
-})
+	plugins: [react()],
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+			"@app": path.resolve(__dirname, "./src/app"),
+			"@pages": path.resolve(__dirname, "./src/pages"),
+			"@widgets": path.resolve(__dirname, "./src/widgets"),
+			"@shared": path.resolve(__dirname, "./src/shared"),
+			"@apis": path.resolve(__dirname, "./src/shared/apis"),
+			"@svg": path.resolve(__dirname, "./src/shared/assets/svg"),
+			"@img": path.resolve(__dirname, "./src/shared/assets/img"),
+			"@styles": path.resolve(__dirname, "./src/shared/styles"),
+			"@libs": path.resolve(__dirname, "./src/shared/libs"),
+			"@configs": path.resolve(__dirname, "./src/shared/configs"),
+			"@ui": path.resolve(__dirname, "./src/shared/ui"),
+		},
+	},
+});
