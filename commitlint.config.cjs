eb4f52427ab2ec111b579ddf3bf3a9066abf1bd7
wonpark1 <2023111927@dgu.ac.kr>
2026-01-04 12:30:22 +0900

    chore: 팀 컨벤션에 맞게 commitlint수정

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,15 +1,34 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
+  parserPreset: {
+    parserOpts: {
+      // allow leading "!" and spaces in type (e.g., "!BREAKING CHANGE")
+      headerPattern: /^(!?[A-Za-z ]+)(?:\(([^)]+)\))?(!?): (.+)$/,
+      headerCorrespondence: ['type', 'scope', 'breaking', 'subject'],
+    },
+  },
   rules: {
     'type-enum': [
       2,
       'always',
       [
-        'feat', 'fix', 'bug', 'refactor', 'design', 'style', 'docs', 'test', 'settings',
-        'chore', 'init', 'rename', 'remove', 'build', 'deploy', 'ci', 'perf', 'revert', 'merge',
+        'init',
+        'feat',
+        'fix',
+        'style',
+        'api',
+        'refactor',
+        'chore',
+        'deploy',
+        'test',
+        'rename',
+        'remove',
+        'docs',
+        '!HOTFIX',
+        '!BREAKING CHANGE',
       ],
     ],
-    'type-case': [2, 'always', 'lower-case'], 
-    'subject-empty': [2, 'never'], 
+    'type-case': [0, 'always'], // allow uppercase + leading ! types
+    'subject-empty': [2, 'never'],
   },
 };
