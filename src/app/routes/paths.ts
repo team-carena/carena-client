e1fe11d887c21443e9b1b55a3fcf586e8d60d954
mimizae <jim400934@gmail.com>
2026-01-06 00:35:20 +0900

    init: 라우터 구조 분리 및 기본 라우팅 구성

--- src/app/routes/paths.ts
+++ src/app/routes/paths.ts
@@ -1,1 +1,8 @@
-// 경로 상수
+export const routePath = {
+	LAYOUT: "/",
+	HOME: "/home",
+	MY_PAGE: "/my-page",
+	// 페이지 작업 시 추가
+} as const;
+
+export type Routes = (typeof routePath)[keyof typeof routePath];
---
+++ src/app/routes/router.tsx
@@ -0,0 +1,20 @@
+import { createBrowserRouter, Navigate } from "react-router";
+import { Layout } from "./layout";
+import { routePath } from "./paths";
+
+export const router = createBrowserRouter([
+	{
+		path: routePath.LAYOUT,
+		element: <Layout />,
+		children: [
+			{
+				index: true,
+				element: <Navigate to={routePath.HOME} replace />,
+			},
+			{
+				path: routePath.MY_PAGE,
+				element: <></>,
+			},
+		],
+	},
+]);
