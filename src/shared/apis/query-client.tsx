125551672eedf323ccefa82404ead8b94341ddaa
mimizae <jim400934@gmail.com>
2026-01-11 18:25:28 +0900

    chore: 안 쓰는 변수 제거

--- src/shared/apis/query-client.tsx
+++ src/shared/apis/query-client.tsx
@@ -3,13 +3,13 @@ import { MutationCache, QueryCache, QueryClient } from "@tanstack/react-query";
 export const createQueryClient = () => {
 	return new QueryClient({
 		queryCache: new QueryCache({
-			onError: (error) => {
+			onError: () => {
 				// TODO: 추후 query 요청 실패 시 공통 에러 처리 로직 추가 예정
 				// ex) 에러 코드별 메시지 매핑, 전역 토스트 노출 등
 			},
 		}),
 		mutationCache: new MutationCache({
-			onError: (error) => {
+			onError: () => {
 				// TODO: mutation 실패 시 공통 에러 처리 로직 추가 예정
 			},
 		}),
