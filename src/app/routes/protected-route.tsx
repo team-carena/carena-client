165b3c96834b0a5b5e591b5b7c36e80304fc42a0
mimizae <jim400934@gmail.com>
2026-01-11 16:12:09 +0900

    chore: 경로 수정

--- src/app/routes/protected-route.tsx
+++ src/app/routes/protected-route.tsx
@@ -4,7 +4,7 @@ export const ProtectedRoute = () => {
 	const isAuthenticated = true; // 임시, 추후 로그인 로직과 연동해 사용자가 인증되었는지 확인 필요
 
 	if (!isAuthenticated) {
-		return <Navigate to="/" replace={true} />;
+		return <Navigate to="/login" replace={true} />;
 	}
 
 	return <Outlet />;
