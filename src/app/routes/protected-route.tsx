89ed0334e7f2ff16826777c2a49b4a97bc46a604
mimizae <jim400934@gmail.com>
2026-01-11 15:40:23 +0900

    feat: 인증 라우트, ProtectedRoute 추가

---
+++ src/app/routes/protected-route.tsx
@@ -0,0 +1,11 @@
+import { Navigate, Outlet } from "react-router";
+
+export const ProtectedRoute = () => {
+	const isAuthenticated = true; // 임시, 추후 로그인 로직과 연동해 사용자가 인증되었는지 확인 필요
+
+	if (!isAuthenticated) {
+		return <Navigate to="/" replace={true} />;
+	}
+
+	return <Outlet />;
+};
