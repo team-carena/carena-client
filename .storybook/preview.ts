ed86a0a0358ff0579320fc394e6074e4f9e8a51c
eojindesu <eojin2820@gmail.com>
2026-01-11 13:12:25 +0900

    chore: storybook autodocs tags 위치 수정

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -2,6 +2,8 @@ import type { Preview } from "@storybook/react-vite";
 import "@app/styles/global.css";
 
 const preview: Preview = {
+	// 자동 문서화
+	tags: ["autodocs"],
 	parameters: {
 		controls: {
 			matchers: {
@@ -9,8 +11,6 @@ const preview: Preview = {
 				date: /Date$/i,
 			},
 		},
-		// 자동 문서화
-		tags: ["autodocs"],
 	},
 };
 
