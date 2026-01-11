c9e38c7b8476e9a4104ffe4dfa985f6d063443f5
mimizae <jim400934@gmail.com>
2026-01-11 18:23:11 +0900

    refactor: queryCache, mutationCache 추가

--- src/shared/apis/query-client.tsx
+++ src/shared/apis/query-client.tsx
@@ -1,7 +1,18 @@
-import { QueryClient } from "@tanstack/react-query";
+import { MutationCache, QueryCache, QueryClient } from "@tanstack/react-query";
 
 export const createQueryClient = () => {
 	return new QueryClient({
+		queryCache: new QueryCache({
+			onError: (error) => {
+				// TODO: 추후 query 요청 실패 시 공통 에러 처리 로직 추가 예정
+				// ex) 에러 코드별 메시지 매핑, 전역 토스트 노출 등
+			},
+		}),
+		mutationCache: new MutationCache({
+			onError: (error) => {
+				// TODO: mutation 실패 시 공통 에러 처리 로직 추가 예정
+			},
+		}),
 		defaultOptions: {
 			queries: {
 				staleTime: 60 * 1000,
