d9395808dcede8318af54958cbf3fa69a01d15cf
jstar_00 <wltjd6300@naver.com>
2026-01-12 07:23:28 +0900

    feat: badge 컴포넌트 구현 (large, small)

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,14 @@
 import "@app/styles/global.css";
 import { ErrorBoundary } from "react-error-boundary";
-import { AppProvider } from "./providers/app-provider";
+import { LargeBadge } from "@/shared/ui/badge/large-badge";
+// import { SmallBadge } from "@/shared/ui/badge/small-badge";
+// import { AppProvider } from "./providers/app-provider";
 import { GlobalErrorFallback } from "./ui/global-error-fallback";
 
 export const App = () => {
 	return (
 		<ErrorBoundary FallbackComponent={GlobalErrorFallback}>
-			<AppProvider />
+			<LargeBadge variant="normal">정상A</LargeBadge>
 		</ErrorBoundary>
 	);
 };
