02cde214876910ab003b934c5b76ca5122403bc4
mimizae <jim400934@gmail.com>
2026-01-13 04:28:18 +0900

    refactor: peer-disabled:[&>span 사용으로 수정해 일관성 유지

--- src/shared/ui/radio-button/radio-button.tsx
+++ src/shared/ui/radio-button/radio-button.tsx
@@ -32,16 +32,14 @@ export const RadioButton = ({
 			/>
 
 			<span
-				data-disabled={disabled}
 				className="
 					w-[2rem] h-[2rem] rounded-full flex items-center justify-center flex-shrink-0
 					border border-gray-700
 					peer-checked:border-primary-500 
 					peer-disabled:border-gray-500 peer-disabled:bg-gray-200 peer-disabled:cursor-not-allowed
-					peer-checked:peer-disabled:border-gray-500
-					peer-checked:peer-disabled:bg-gray-100
+					peer-checked:peer-disabled:border-gray-500 peer-checked:peer-disabled:bg-gray-100
 					[&>span]:opacity-0 peer-checked:[&>span]:opacity-100
-					data-[disabled=true]:[&>span]:bg-gray-500
+					peer-disabled:[&>span]:bg-gray-500
 				"
 			>
 				<span className="w-[1rem] h-[1rem] rounded-full bg-primary-500" />
