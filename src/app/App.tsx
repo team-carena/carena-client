4e200eeae7cfbb7188de5992eaf3ab171003853d
jstar_00 <wltjd6300@naver.com>
2026-01-12 09:21:04 +0900

    feat: tag 컴포넌트 구현

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,6 +2,7 @@ import "@app/styles/global.css";
 import { ErrorBoundary } from "react-error-boundary";
 import { LargeBadge } from "@/shared/ui/badges/large-badge";
 import Chip from "@/shared/ui/chips/chip";
+import Tag from "@/shared/ui/tags/tag";
 // import { SmallBadge } from "@/shared/ui/badge/small-badge";
 // import { AppProvider } from "./providers/app-provider";
 import { GlobalErrorFallback } from "./ui/global-error-fallback";
@@ -9,7 +10,7 @@ import { GlobalErrorFallback } from "./ui/global-error-fallback";
 export const App = () => {
 	return (
 		<ErrorBoundary FallbackComponent={GlobalErrorFallback}>
-			<Chip status="on">생활습관</Chip>
+			<Tag>#태그</Tag>
 		</ErrorBoundary>
 	);
 };
