f58d63c1d1caf44a126600b4a7cbd4fcd5cfa2af
mimizae <jim400934@gmail.com>
2026-01-12 22:17:54 +0900

    feat: ocr-button ui 구현

---
+++ src/shared/ui/buttons/ocr-button.tsx
@@ -0,0 +1,9 @@
+export const OcrButton = () => {
+	return (
+		<button className="flex w-[23.6rem] m-w-[8.4rem] px-[1.6rem] py-[1.2rem] items-center justify-center gap-[1.2rem] rounded-[0.8rem] border border-primary-600 bg-primary-50">
+			<p className="label03-m-12 text-primary-600">
+				검진 결과지 스캔하고 자동 입력받기
+			</p>
+		</button>
+	);
+};
