61b16dd149599360260314e7f940aa6287d5c549
mimizae <jim400934@gmail.com>
2026-01-12 23:13:24 +0900

    refactor: ocr-button props를 ButtonHTMLAttributes로 확장

--- src/shared/ui/buttons/ocr-button.tsx
+++ src/shared/ui/buttons/ocr-button.tsx
@@ -1,6 +1,20 @@
-export const OcrButton = () => {
+import { Scan } from "@/shared/assets/svg";
+import { cn } from "@/shared/libs/cn";
+
+interface OcrButtonProps
+	extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
+
+export const OcrButton = ({ onClick, className, ...props }: OcrButtonProps) => {
 	return (
-		<button className="flex w-[23.6rem] m-w-[8.4rem] px-[1.6rem] py-[1.2rem] items-center justify-center gap-[1.2rem] rounded-[0.8rem] border border-primary-600 bg-primary-50">
+		<button
+			onClick={onClick}
+			{...props}
+			className={cn(
+				"flex w-full min-w-[8.4rem] px-[1.6rem] py-[1.2rem] items-center justify-center gap-[1.2rem] rounded-[0.8rem] border border-primary-600 bg-primary-50 active:bg-primary-100",
+				className,
+			)}
+		>
+			<Scan />
 			<p className="label03-m-12 text-primary-600">
 				검진 결과지 스캔하고 자동 입력받기
 			</p>
