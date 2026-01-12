cf44918bdd0d7a6c8e2555e0028e214892cc6e57
mimizae <jim400934@gmail.com>
2026-01-13 02:44:22 +0900

    feat: 라디오 버튼에 text 있는 버전 추가

--- src/shared/ui/radio-button/radio-button.tsx
+++ src/shared/ui/radio-button/radio-button.tsx
@@ -1,4 +1,7 @@
+import { clsx } from "clsx";
+
 interface RadioButtonProps {
+	text?: string;
 	checked: boolean;
 	onChange: () => void;
 	name: string;
@@ -6,13 +9,19 @@ interface RadioButtonProps {
 }
 
 export const RadioButton = ({
+	text,
 	checked,
 	onChange,
 	name,
 	disabled,
 }: RadioButtonProps) => {
 	return (
-		<label className="inline-flex items-center cursor-pointer">
+		<label
+			className={clsx(
+				"w-fit flex items-center cursor-pointer",
+				text && "gap-[1.2rem] py-[1.2rem] pl-[1.2rem]",
+			)}
+		>
 			<input
 				type="radio"
 				name={name}
@@ -25,21 +34,20 @@ export const RadioButton = ({
 			<span
 				data-disabled={disabled}
 				className="
-          w-[2rem] h-[2rem] rounded-full flex items-center justify-center
-          border border-gray-700
-
-          peer-checked:border-primary-500 
-          peer-disabled:border-gray-500 peer-disabled:bg-gray-200 peer-disabled:cursor-not-allowed
-          
-          peer-checked:peer-disabled:border-gray-500
-          peer-checked:peer-disabled:bg-gray-100
-          
-          [&>span]:opacity-0 peer-checked:[&>span]:opacity-100
-          data-[disabled=true]:[&>span]:bg-gray-500
-        "
+					w-[2rem] h-[2rem] rounded-full flex items-center justify-center flex-shrink-0
+					border border-gray-700
+					peer-checked:border-primary-500 
+					peer-disabled:border-gray-500 peer-disabled:bg-gray-200 peer-disabled:cursor-not-allowed
+					peer-checked:peer-disabled:border-gray-500
+					peer-checked:peer-disabled:bg-gray-100
+					[&>span]:opacity-0 peer-checked:[&>span]:opacity-100
+					data-[disabled=true]:[&>span]:bg-gray-500
+				"
 			>
 				<span className="w-[1rem] h-[1rem] rounded-full bg-primary-500" />
 			</span>
+
+			{text && <span className="label04-r-16 text-black">{text}</span>}
 		</label>
 	);
 };
