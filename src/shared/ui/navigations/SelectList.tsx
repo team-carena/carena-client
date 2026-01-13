c87a7dee5a8888b9afbedab0984ab8b047588991
eojindesu <eojin2820@gmail.com>
2026-01-14 03:21:06 +0900

    feat: select list 공통 컴포넌트 구현

---
+++ src/shared/ui/navigations/SelectList.tsx
@@ -0,0 +1,39 @@
+import { ChevronSRight } from "@shared/assets/svg";
+import { cn } from "@shared/libs/cn";
+import { cva } from "class-variance-authority";
+import * as React from "react";
+
+const selectListVariants = cva(
+	[
+		"flex items-center justify-between",
+		"w-[29.5rem] h-[2.8rem]",
+		"px-0 py-[0.4rem]",
+		"cursor-pointer",
+		"text-gray-900",
+	].join(" "),
+);
+
+export interface SelectListProps extends React.HTMLAttributes<HTMLDivElement> {
+	label: string;
+}
+
+export const SelectList = React.forwardRef<HTMLDivElement, SelectListProps>(
+	({ label, className, ...props }, ref) => {
+		return (
+			<div ref={ref} className={cn(selectListVariants(), className)} {...props}>
+				{/* text */}
+				<span className="flex-[1_0_0] body04-r-14">{label}</span>
+
+				{/* icon */}
+				<ChevronSRight
+					className="shrink-0 text-gray-900"
+					width={20}
+					height={20}
+					aria-hidden
+				/>
+			</div>
+		);
+	},
+);
+
+SelectList.displayName = "SelectList";
