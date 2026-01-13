96f6aae3b169e405e2433f5fe76a507091a875c7
mimizae <jim400934@gmail.com>
2026-01-14 04:00:30 +0900

    refactor: 시맨틱 태그 추가

--- src/shared/ui/buttons/add-button.tsx
+++ src/shared/ui/buttons/add-button.tsx
@@ -5,18 +5,17 @@ import { cn } from "@/shared/libs/cn";
 interface AddButtonProps
 	extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
 
-export const AddButton = ({ onClick, className, ...props }: AddButtonProps) => {
+export const AddButton = ({ className, ...props }: AddButtonProps) => {
 	return (
 		<button
 			type="button"
-			onClick={onClick}
 			{...props}
 			className={cn(
 				"w-fit flex px-[0.8rem] py-[0.4rem] items-center justify-center gap-[0.4rem] rounded-[4px] bg-white active:bg-gray-200 transition-default",
 				className,
 			)}
 		>
-			<p className="label06-r-12 text-gray-900">검진결과추가</p>
+			<span className="label06-r-12 text-gray-900">검진결과추가</span>
 			<Plus />
 		</button>
 	);
