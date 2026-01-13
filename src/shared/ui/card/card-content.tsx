3a70c3014848735f23763322c093c03a41ce8072
wonpark1 <2023111927@dgu.ac.kr>
2026-01-14 03:40:46 +0900

    chore: rem 단위 수정

--- src/shared/ui/card/card-content.tsx
+++ src/shared/ui/card/card-content.tsx
@@ -8,14 +8,14 @@ export type ContentCardProps = {
 
 const ContentCard = ({ subtitle, content, tags }: ContentCardProps) => {
 	return (
-		<div className="w-full rounded-[12px] border border-gray-100 bg-white px-[20px] py-[24px]">
+		<div className="w-full rounded-[1.2rem] border border-gray-100 bg-white px-[2rem] py-[2.4rem]">
 			{subtitle ? (
-				<h3 className="head04-m-16 mb-[8px] text-gray-900">{subtitle}</h3>
+				<h3 className="head04-m-16 mb-[0.8rem] text-gray-900">{subtitle}</h3>
 			) : null}
 
 			<p className="body03-r-16 text-gray-900">{content}</p>
 
-			<div className="mt-[20px] flex flex-wrap gap-[8px]">
+			<div className="mt-[2rem] flex flex-wrap gap-[0.8rem]">
 				{tags.map((tag) => (
 					<Tag key={tag}>{tag}</Tag>
 				))}
