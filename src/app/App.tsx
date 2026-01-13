d7eec247ce2c28aec8bef5213e43a25789b77e0b
jstar_00 <wltjd6300@naver.com>
2026-01-13 21:51:23 +0900

    refactor: Toaster provider 위치 변경

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,5 @@
 import "@app/styles/global.css";
 import { ErrorBoundary } from "react-error-boundary";
-import { MainToaster } from "@/shared/ui/toasts/toast";
 import { AppProvider } from "./providers/app-provider";
 import { GlobalErrorFallback } from "./ui/global-error-fallback";
 
@@ -8,7 +7,6 @@ export const App = () => {
 	return (
 		<ErrorBoundary FallbackComponent={GlobalErrorFallback}>
 			<AppProvider />
-			<MainToaster />
 		</ErrorBoundary>
 	);
 };
--- src/app/providers/app-provider.tsx
+++ src/app/providers/app-provider.tsx
@@ -1,3 +1,4 @@
+import { MainToaster } from "@/shared/ui/toasts/toast";
 import { QueryProvider } from "./query-provider";
 import { RouterProvider } from "./router-provider";
 
@@ -5,6 +6,7 @@ export const AppProvider = () => {
 	return (
 		<QueryProvider>
 			<RouterProvider />
+			<MainToaster />
 		</QueryProvider>
 	);
 };
