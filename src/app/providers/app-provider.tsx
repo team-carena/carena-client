77756a8a8dfe6d95eaa6fa7ce6875ac7851c8031
jstar_00 <wltjd6300@naver.com>
2026-01-13 15:02:45 +0900

    fix: OverlayProvider 위치 AppProvider로 이동

--- src/app/providers/app-provider.tsx
+++ src/app/providers/app-provider.tsx
@@ -1,10 +1,13 @@
+import { OverlayProvider } from "overlay-kit";
 import { QueryProvider } from "./query-provider";
 import { RouterProvider } from "./router-provider";
 
 export const AppProvider = () => {
 	return (
 		<QueryProvider>
-			<RouterProvider />
+			<OverlayProvider>
+				<RouterProvider />
+			</OverlayProvider>
 		</QueryProvider>
 	);
 };
