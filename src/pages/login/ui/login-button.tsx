f8dc501b811fa9adf943c36e2d5bdc861790c401
mimizae <jim400934@gmail.com>
2026-01-15 03:06:02 +0900

    feat: 로그인 버튼에 onClick 등록

--- src/pages/login/ui/login-button.tsx
+++ src/pages/login/ui/login-button.tsx
@@ -2,11 +2,18 @@ import { KakaoIcon } from "@/shared/assets/svg";
 
 interface LoginButtonProps {
 	type?: string;
+	onClick: () => void;
 }
 
-export const LoginButton = ({ type = "카카오톡" }: LoginButtonProps) => {
+export const LoginButton = ({
+	type = "카카오톡",
+	onClick,
+}: LoginButtonProps) => {
 	return (
-		<button className="flex w-full min-w-[33.5rem] gap-[1.2rem] items-center px-[1.6rem] py-[1.2rem] rounded-[8px] bg-[#FEE500]">
+		<button
+			onClick={onClick}
+			className="flex w-full min-w-[33.5rem] gap-[1.2rem] items-center px-[1.6rem] py-[1.2rem] rounded-[8px] bg-[#FEE500]"
+		>
 			<KakaoIcon />
 			<span className="body03-r-16">{type}으로 시작하기</span>
 		</button>
--- src/pages/login/ui/login.tsx
+++ src/pages/login/ui/login.tsx
@@ -1,10 +1,10 @@
 import Lottie from "react-lottie-player";
 import LoginBackGround from "@/shared/assets/img/login-bg.png";
-import { CarenaLogoLogin } from "@/shared/assets/svg";
+import { CarenaLogoText } from "@/shared/assets/svg";
 import LandingGraphic from "../landing-graphic.json";
 import { LoginButton } from "./login-button";
 
-export const Login = () => {
+export const LoginPage = () => {
 	return (
 		<div
 			className="min-h-screen bg-center bg-no-repeat bg-cover px-[2rem]"
@@ -12,7 +12,6 @@ export const Login = () => {
 		>
 			<div className="flex flex-col gap-[1.9rem] pt-[4rem] pb-[1.6rem] px-[0.4rem]">
 				<span className="display03-sb-20 text-gray-900">
-					{" "}
 					검진 결과 이후의 모든 순간을
 				</span>
 				<span className="display01-b-36 text-primary-500">더 쉽게</span>
@@ -20,17 +19,18 @@ export const Login = () => {
 
 			<Lottie
 				animationData={LandingGraphic}
+				loop
 				play
-				className="w-full h-[20rem] max-w-[320px] mx-auto"
+				className="w-[20rem] h-[22rem] mx-auto mt-[8.1rem] mb-[7.2rem]"
 			/>
 
 			<div className="flex flex-col gap-[0.4rem] mb-[4rem] text-gray-900 body03-r-16">
-				<span>검진 결과를 입력하면,</span>
-				<span className="flex items-center gap-[0.4rem] h-[2.2rem]">
-					<CarenaLogoLogin />가 해설부터 관리까지 도와드려요.
+				검진 결과를 입력하면,
+				<span className="flex gap-[0.4rem] h-[2.2rem] items-center">
+					<CarenaLogoText />가 해설부터 관리까지 도와드려요.
 				</span>
 			</div>
-			<LoginButton />
+			<LoginButton onClick={() => {}} />
 		</div>
 	);
 };
