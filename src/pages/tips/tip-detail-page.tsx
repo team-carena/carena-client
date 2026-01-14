8d1c374fbde104a8b88b5223d70788e15a9a5bc0
wonpark1 <2023111927@dgu.ac.kr>
2026-01-14 16:44:40 +0900

    feat:  페이지 기본 레이아웃 추가

---
+++ src/pages/tips/tip-detail-page.tsx
@@ -0,0 +1,14 @@
+import CardTip from "@shared/ui/card/card-tip";
+
+export const TipDetailPage = () => {
+	return (
+		<div className="w-[37.5rem] h-[81.2rem] bg-white border-gray-200 border">
+			<div className="h-[5.6rem] w-[37.5rem] border-b border-gray-200" />
+			<div className="px-[2rem] py-[2.4rem] bg-gray-50">
+				<div>
+					<CardTip>팁 상세 내용이 들어갑니다.</CardTip>
+				</div>
+			</div>
+		</div>
+	);
+};
