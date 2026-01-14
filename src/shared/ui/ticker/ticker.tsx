b1c946ff3448cac049e0f9ebc48645f9a590990a
mimizae <jim400934@gmail.com>
2026-01-14 16:28:02 +0900

    refactor: 모호한 변수명 명확히 변경, 건강 팁 타입 변경

--- src/shared/ui/ticker/ticker.tsx
+++ src/shared/ui/ticker/ticker.tsx
@@ -1,56 +1,58 @@
 import { useEffect, useState } from "react";
+import { cn } from "@/shared/libs/cn";
+
+interface Tips {
+	id: number;
+	title: string;
+} // TODO: 건강 팁 api 응답 타입을 export 해오기
 
 interface TickerProps {
-	tips: string[];
+	tips: Tips[];
 }
 
 export const Ticker = ({ tips }: TickerProps) => {
-	const [currentIndex, setCurrentIndex] = useState(0);
-	const itemHeight = 35;
+	const [visibleIndex, setVisibleIndex] = useState(0);
+	const [isTransitionEnabled, setIsTransitionEnabled] = useState(false);
 
 	const extendedTips = [...tips, tips[0]];
+	const ITEM_HEIGHT = 35;
 
-	// 계속 내려가게 만드는 핵심...
 	useEffect(() => {
-		const interval = setInterval(() => {
-			setCurrentIndex((prev) => prev + 1);
+		const autoScrollInterval = setInterval(() => {
+			setIsTransitionEnabled(true);
+			setVisibleIndex((prev) => prev + 1);
 		}, 3000);
 
-		return () => clearInterval(interval);
+		return () => clearInterval(autoScrollInterval);
 	}, []);
 
-	// 끝에서 순간이동...
 	useEffect(() => {
-		if (currentIndex === tips.length) {
-			const timer = setTimeout(() => {
-				setCurrentIndex(0);
+		if (visibleIndex === tips.length) {
+			const resetIndexTimeout = setTimeout(() => {
+				setIsTransitionEnabled(false);
+				setVisibleIndex(0);
 			}, 400);
 
-			return () => clearTimeout(timer);
+			return () => clearTimeout(resetIndexTimeout);
 		}
-	}, [currentIndex, tips.length]);
+	}, [visibleIndex, tips.length]);
 
 	return (
-		<div
-			className="border w-full px-[1.2rem] py-[0.8rem] rounded-[8px] bg-white label04-r-16 overflow-hidden"
-			style={{ height: itemHeight }}
-		>
+		<div className="w-full h-[3.5rem] overflow-hidden px-[1.2rem] py-[0.8rem] rounded-[8px] bg-white label04-r-16">
 			<div
-				style={{
-					transform: `translateY(-${currentIndex * itemHeight}px)`,
-					transition: "transform 0.4s ease-in-out",
-				}}
+				className={cn(
+					"will-change-transform",
+					isTransitionEnabled &&
+						"transition-transform duration-400 ease-in-out",
+				)}
+				style={{ transform: `translateY(-${visibleIndex * ITEM_HEIGHT}px)` }}
 			>
-				{extendedTips.map((tip, index) => (
+				{extendedTips.map((tip) => (
 					<div
-						key={`${index}-${tip}`}
-						style={{
-							height: itemHeight,
-							display: "flex",
-							alignItems: "center",
-						}}
+						key={tip.id}
+						className="h-[3.5rem] overflow-hidden text-ellipsis whitespace-nowrap"
 					>
-						{tip}
+						{tip.title}
 					</div>
 				))}
 			</div>
