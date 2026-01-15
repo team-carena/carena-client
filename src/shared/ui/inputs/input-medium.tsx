3f33bf1defc4d0678423b9e104d2066f734d3bc3
eojindesu <eojin2820@gmail.com>
2026-01-16 01:33:39 +0900

    chore: 불필요한 VariantProps 제거

--- src/shared/ui/inputs/input-medium.tsx
+++ src/shared/ui/inputs/input-medium.tsx
@@ -1,6 +1,6 @@
 import { cn } from "@shared/libs/cn";
 import SystemDangerIcon from "@svg/system-danger.svg?react";
-import { cva, type VariantProps } from "class-variance-authority";
+import { cva } from "class-variance-authority";
 import * as React from "react";
 
 const inputMediumVariants = cva(
