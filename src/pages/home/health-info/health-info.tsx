f846483c8468cd21f3529f0751cb7fcbb68bf2c5
jstar_00 <wltjd6300@naver.com>
2026-01-17 03:36:33 +0900

    feat: Tabs 적용 및 건강 팁 nav 컴포넌트

--- src/pages/home/health-info/health-info.tsx
+++ src/pages/home/health-info/health-info.tsx
@@ -1,9 +1,25 @@
-import UserInfo from "./user-info";
+import { NaviRow } from "@/shared/ui/navigations/navi-row";
+import { Ticker } from "@/shared/ui/ticker/ticker";
 
 const HealthInfoPage = () => {
 	return (
-		<div>
-			<UserInfo />
+		<div className="flex flex-col w-full pt-[2.4rem] px-[2rem]">
+			<article className="bg-white rounded-[12px]">
+				<div className="p-[1.2rem_1.2rem_0.4rem_1.2rem]">
+					{/* TODO: 건강팁 라우팅 추가 */}
+					<NaviRow label="생활 속 건강 팁" to="" />
+				</div>
+				<div className="px-[0.8rem] py-[1.2rem]">
+					<Ticker
+						tips={[
+							{ id: 1, title: "건강팁 한 줄 쪼로록" },
+							{ id: 2, title: "건강팁 두 줄 쪼로록" },
+							{ id: 3, title: "건강팁 세 줄 쪼로록" },
+						]}
+					/>
+				</div>
+			</article>
+			<article></article>
 		</div>
 	);
 };
