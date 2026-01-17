c3929fb1521eeb5aaec0d6e92149853c5966a662
jstar_00 <wltjd6300@naver.com>
2026-01-17 15:36:55 +0900

    feat: info trigger 적용

--- src/pages/home/health-info/components/user-info.tsx
+++ src/pages/home/health-info/components/user-info.tsx
@@ -19,14 +19,18 @@ const UserInfo = () => {
 
 			{/* 우측 표 */}
 			<div className="flex flex-1 items-center justify-center">
-				<figure>
-					<RadialChart score={90} className="w-[15rem] h-[15rem]" />
-				</figure>
-				<Tooltip side="bottom" align="start">
-					건강점수는 체형, 혈압, 혈당, 간·신장 관련 최근 검진 항목을 점수로
-					변환해 종합한 지표로, 현재 건강 상태를 이해하기 쉽게 보여줍니다.
-					자세한 산출 기준은 보기를 통해 확인할 수 있습니다.
-				</Tooltip>
+				<div className="relative">
+					<figure>
+						<RadialChart score={90} className="w-[15rem] h-[15rem]" />
+					</figure>
+					<div className="absolute bottom-[-7px] right-[-5px]">
+						<Tooltip side="bottom" align="end">
+							건강점수는 체형, 혈압, 혈당, 간·신장 관련 최근 검진 항목을 점수로
+							변환해 종합한 지표로, 현재 건강 상태를 이해하기 쉽게 보여줍니다.
+							자세한 산출 기준은 보기를 통해 확인할 수 있습니다.
+						</Tooltip>
+					</div>
+				</div>
 			</div>
 		</section>
 	);
--- src/shared/ui/overlays/tooltip/popover.tsx
+++ src/shared/ui/overlays/tooltip/popover.tsx
@@ -19,14 +19,17 @@ function PopoverContent({
 	className,
 	align = "center", // 삼각형 위치 (start, center, end)
 	sideOffset = 14, // PopoverTrigger(여기서는 info 아이콘)으로부터 얼마나 떨어져있는가
+	avoidCollisions = true, // 화면 경계 충돌 감지 활성화
 	...props
 }: React.ComponentProps<typeof PopoverPrimitive.Content>) {
 	return (
 		<PopoverPrimitive.Portal>
 			<PopoverPrimitive.Content
 				data-slot="popover-content"
+				data-align={align}
 				align={align}
 				sideOffset={sideOffset}
+				avoidCollisions={avoidCollisions}
 				className={cn(
 					// Popover 내용을 나타내는 영역의 css
 					"group z-50 w-[25.6rem] rounded-[12px] border border-gray-300 bg-white p-[2.4rem] body05-r-12 text-gray-900 whitespace-pre-wrap",
@@ -47,19 +50,31 @@ function PopoverContent({
 					className={cn(
 						"absolute w-0 h-0",
 						// side=top: 삼각형이 아래쪽에 위치 (가로 22px, 세로 12px)
-						"group-data-[side=top]:bottom-[-12px] group-data-[side=top]:left-[24px]",
+						"group-data-[side=top]:bottom-[-12px]",
+						"group-data-[side=top]:group-data-[align=start]:left-[24px]",
+						"group-data-[side=top]:group-data-[align=center]:left-1/2 group-data-[side=top]:group-data-[align=center]:-translate-x-1/2",
+						"group-data-[side=top]:group-data-[align=end]:right-[24px]",
 						"group-data-[side=top]:border-l-[11px] group-data-[side=top]:border-r-[11px] group-data-[side=top]:border-t-[12px]",
 						"group-data-[side=top]:border-l-transparent group-data-[side=top]:border-r-transparent group-data-[side=top]:border-t-gray-300",
 						// side=bottom: 삼각형이 위쪽에 위치
-						"group-data-[side=bottom]:top-[-12px] group-data-[side=bottom]:left-[24px]",
+						"group-data-[side=bottom]:top-[-12px]",
+						"group-data-[side=bottom]:group-data-[align=start]:left-[24px]",
+						"group-data-[side=bottom]:group-data-[align=center]:left-1/2 group-data-[side=bottom]:group-data-[align=center]:-translate-x-1/2",
+						"group-data-[side=bottom]:group-data-[align=end]:right-[24px]",
 						"group-data-[side=bottom]:border-l-[11px] group-data-[side=bottom]:border-r-[11px] group-data-[side=bottom]:border-b-[12px]",
 						"group-data-[side=bottom]:border-l-transparent group-data-[side=bottom]:border-r-transparent group-data-[side=bottom]:border-b-gray-300",
 						// side=left: 삼각형이 오른쪽에 위치 (가로 12px, 세로 22px)
-						"group-data-[side=left]:right-[-12px] group-data-[side=left]:top-[24px]",
+						"group-data-[side=left]:right-[-12px]",
+						"group-data-[side=left]:group-data-[align=start]:top-[24px]",
+						"group-data-[side=left]:group-data-[align=center]:top-1/2 group-data-[side=left]:group-data-[align=center]:-translate-y-1/2",
+						"group-data-[side=left]:group-data-[align=end]:bottom-[24px]",
 						"group-data-[side=left]:border-t-[11px] group-data-[side=left]:border-b-[11px] group-data-[side=left]:border-l-[12px]",
 						"group-data-[side=left]:border-t-transparent group-data-[side=left]:border-b-transparent group-data-[side=left]:border-l-gray-300",
 						// side=right: 삼각형이 왼쪽에 위치
-						"group-data-[side=right]:left-[-12px] group-data-[side=right]:top-[24px]",
+						"group-data-[side=right]:left-[-12px]",
+						"group-data-[side=right]:group-data-[align=start]:top-[24px]",
+						"group-data-[side=right]:group-data-[align=center]:top-1/2 group-data-[side=right]:group-data-[align=center]:-translate-y-1/2",
+						"group-data-[side=right]:group-data-[align=end]:bottom-[24px]",
 						"group-data-[side=right]:border-t-[11px] group-data-[side=right]:border-b-[11px] group-data-[side=right]:border-r-[12px]",
 						"group-data-[side=right]:border-t-transparent group-data-[side=right]:border-b-transparent group-data-[side=right]:border-r-gray-300",
 					)}
@@ -68,20 +83,32 @@ function PopoverContent({
 				<span
 					className={cn(
 						"absolute w-0 h-0",
-						// side=top: 테두리 삼각형보다 1px 안쪽에서 시작 (bottom: -12px + 1px = -11px)
-						"group-data-[side=top]:bottom-[-11px] group-data-[side=top]:left-[25px]",
+						// side=top: 테두리 삼각형보다 1px 안쪽에서 시작
+						"group-data-[side=top]:bottom-[-11px]",
+						"group-data-[side=top]:group-data-[align=start]:left-[25px]",
+						"group-data-[side=top]:group-data-[align=center]:left-1/2 group-data-[side=top]:group-data-[align=center]:-translate-x-1/2",
+						"group-data-[side=top]:group-data-[align=end]:right-[25px]",
 						"group-data-[side=top]:border-l-[10px] group-data-[side=top]:border-r-[10px] group-data-[side=top]:border-t-[12px]",
 						"group-data-[side=top]:border-l-transparent group-data-[side=top]:border-r-transparent group-data-[side=top]:border-t-white",
 						// side=bottom
-						"group-data-[side=bottom]:top-[-11px] group-data-[side=bottom]:left-[25px]",
+						"group-data-[side=bottom]:top-[-11px]",
+						"group-data-[side=bottom]:group-data-[align=start]:left-[25px]",
+						"group-data-[side=bottom]:group-data-[align=center]:left-1/2 group-data-[side=bottom]:group-data-[align=center]:-translate-x-1/2",
+						"group-data-[side=bottom]:group-data-[align=end]:right-[25px]",
 						"group-data-[side=bottom]:border-l-[10px] group-data-[side=bottom]:border-r-[10px] group-data-[side=bottom]:border-b-[12px]",
 						"group-data-[side=bottom]:border-l-transparent group-data-[side=bottom]:border-r-transparent group-data-[side=bottom]:border-b-white",
 						// side=left
-						"group-data-[side=left]:right-[-11px] group-data-[side=left]:top-[25px]",
+						"group-data-[side=left]:right-[-11px]",
+						"group-data-[side=left]:group-data-[align=start]:top-[25px]",
+						"group-data-[side=left]:group-data-[align=center]:top-1/2 group-data-[side=left]:group-data-[align=center]:-translate-y-1/2",
+						"group-data-[side=left]:group-data-[align=end]:bottom-[25px]",
 						"group-data-[side=left]:border-t-[10px] group-data-[side=left]:border-b-[10px] group-data-[side=left]:border-l-[12px]",
 						"group-data-[side=left]:border-t-transparent group-data-[side=left]:border-b-transparent group-data-[side=left]:border-l-white",
 						// side=right
-						"group-data-[side=right]:left-[-11px] group-data-[side=right]:top-[25px]",
+						"group-data-[side=right]:left-[-11px]",
+						"group-data-[side=right]:group-data-[align=start]:top-[25px]",
+						"group-data-[side=right]:group-data-[align=center]:top-1/2 group-data-[side=right]:group-data-[align=center]:-translate-y-1/2",
+						"group-data-[side=right]:group-data-[align=end]:bottom-[25px]",
 						"group-data-[side=right]:border-t-[10px] group-data-[side=right]:border-b-[10px] group-data-[side=right]:border-r-[12px]",
 						"group-data-[side=right]:border-t-transparent group-data-[side=right]:border-b-transparent group-data-[side=right]:border-r-white",
 					)}
