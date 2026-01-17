e7c7c841bf1f774a79144ed9ed490b788da638c3
jstar_00 <wltjd6300@naver.com>
2026-01-17 21:43:13 +0900

    chore: 불필요한 코드 제거

--- src/pages/home/ui/home.tsx
+++ src/pages/home/ui/home.tsx
@@ -1,23 +1,3 @@
-import { Tabs } from "@/shared/ui/tabs/tabs";
-import HealthInfoPage from "../health-info/health-info";
-import UserInfo from "../health-info/user-info";
-
 export const HomePage = () => {
-	return (
-		<div className="flex flex-col w-full">
-			<UserInfo />
-			<Tabs defaultTab="health-info">
-				<Tabs.List>
-					<Tabs.Trigger value="health-info">건강정보</Tabs.Trigger>
-					<Tabs.Trigger value="health-tips">검진결과분석</Tabs.Trigger>
-				</Tabs.List>
-				<Tabs.Content value="health-info">
-					<HealthInfoPage />
-				</Tabs.Content>
-				<Tabs.Content value="health-tips">
-					<div>검진결과분석 내용입니다.</div>
-				</Tabs.Content>
-			</Tabs>
-		</div>
-	);
+	return <div></div>;
 };
