29efac62372efd7b5264e897171a2b56b2e8a9d8
mimizae <jim400934@gmail.com>
2026-01-18 22:20:40 +0900

    chore: refresh 함수 주석처리

--- src/app/auth-initializer.tsx
+++ src/app/auth-initializer.tsx
@@ -1,30 +1,29 @@
-import { useEffect } from "react";
-import { apiClient } from "@/shared/apis/api-client";
+import { type ReactNode, useEffect } from "react";
 import { useAuthStore } from "@/shared/store/auth-store";
 
-export const AuthInitializer = ({
-	children,
-}: {
-	children: React.ReactNode;
-}) => {
+type AuthInitializerProps = {
+	children: ReactNode;
+};
+
+export const AuthInitializer = ({ children }: AuthInitializerProps) => {
 	const { setAccessToken, setAuthenticated, setAuthCheckLoading } =
 		useAuthStore();
 
 	useEffect(() => {
 		const initializeAuth = async () => {
 			try {
-				const res = await apiClient.post("/auth/refresh"); // TODO: 임시, 추후 api 함수로 대체
-				setAccessToken(res.data.accessToken);
+				//const res = await apiClient.post("/auth/refresh");
+				setAccessToken("");
 				setAuthenticated(true); // 성공
 			} catch {
 				setAuthenticated(false); // 실패
 			} finally {
-				setAuthCheckLoading(false); // 확정 (결과 나옴)
+				setAuthCheckLoading(false); // 확정
 			}
 		};
 
 		void initializeAuth();
 	}, [setAccessToken, setAuthenticated, setAuthCheckLoading]);
 
-	return <div>{children}</div>;
+	return <>{children}</>;
 };
