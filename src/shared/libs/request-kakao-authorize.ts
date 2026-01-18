872f4b93e89f2c746a2bd6cfb04ce7ee31920c8e
mimizae <jim400934@gmail.com>
2026-01-18 22:18:25 +0900

    chore: 주석 위치 수정

--- src/shared/libs/request-kakao-authorize.ts
+++ src/shared/libs/request-kakao-authorize.ts
@@ -1,10 +1,9 @@
 const KAKAO_REST_API_KEY = import.meta.env.VITE_KAKAO_REST_API_KEY;
 const KAKAO_REDIRECT_URI = import.meta.env.VITE_KAKAO_REDIRECT_URI;
 
+// 카카오 인가 코드 요청 유틸함수
 export const requestKakaoAuthorize = () => {
 	const url = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${KAKAO_REST_API_KEY}&redirect_uri=${KAKAO_REDIRECT_URI}`;
 
 	window.location.href = url;
 };
-
-// 카카오 인가 코드 요청 유틸함수
