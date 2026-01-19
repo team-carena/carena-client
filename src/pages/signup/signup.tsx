1c22bbf58c5e35b3425db790bc85fb62a2b6d0f0
jstar_00 <wltjd6300@naver.com>
2026-01-19 12:21:52 +0900

    init: Tailwind 클래스 자동정렬 설정 추가

--- src/pages/signup/signup.tsx
+++ src/pages/signup/signup.tsx
@@ -1,7 +1,22 @@
+interface SignupCategoryProps {
+	label: string;
+}
+
+const SignupCategory = ({ label }: SignupCategoryProps) => {
+	return (
+		<h3 className="head02-b-16 mb-[2rem] text-left text-primary-700">
+			{label}
+		</h3>
+	);
+};
+
 const Signup = () => {
 	return (
 		<div>
-			<div></div>
+			{/* 입력 컴포넌트 */}
+			<div className="flex min-h-dvh w-full flex-col gap-[4rem] bg-white px-[2rem] pt-[4rem] pb-[2.4rem]">
+				<SignupCategory label={"기본정보"} />
+			</div>
 		</div>
 	);
 };
