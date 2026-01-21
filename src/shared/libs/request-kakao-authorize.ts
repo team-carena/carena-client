ebfa175df5940bcc0b8955a2d0ee455d1b4ba0d5
jstar_00 <wltjd6300@naver.com>
2026-01-21 21:45:02 +0900

    fix: 미사용 환경변수 제거

--- src/shared/libs/request-kakao-authorize.ts
+++ src/shared/libs/request-kakao-authorize.ts
@@ -1,16 +1,5 @@
-const KAKAO_REST_API_KEY = import.meta.env.VITE_KAKAO_REST_API_KEY;
-const KAKAO_REDIRECT_URI = import.meta.env.VITE_KAKAO_REDIRECT_URI;
-
 // 카카오 인가 코드 요청 유틸함수
 export const requestKakaoAuthorize = () => {
-	if (!KAKAO_REST_API_KEY || !KAKAO_REDIRECT_URI) {
-		throw new Error("Kakao OAuth 환경변수가 설정되지 않았습니다.");
-	}
-
 	const url = "https://api.care-na.com/oauth2/authorization/kakao";
-	// `https://kauth.kakao.com/oauth/authorize?response_type=code` +
-	// `&client_id=${encodeURIComponent(KAKAO_REST_API_KEY)}` +
-	// `&redirect_uri=${encodeURIComponent(KAKAO_REDIRECT_URI)}`;
-
 	window.location.assign(url);
 };
