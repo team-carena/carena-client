7422fd55898edbce0ecaf6f87b5622863e205db0
jstar_00 <wltjd6300@naver.com>
2026-01-22 21:22:26 +0900

    feat: RecommendedMeal API 구현

---
+++ src/pages/home/apis/queries/use-recommended-meals.ts
@@ -0,0 +1,19 @@
+import { useSuspenseQuery } from "@tanstack/react-query";
+import { API_ENDPOINTS } from "@/shared/apis/api-endpoints";
+import type { RecommendedMealView } from "@/shared/apis/generated/data-contracts";
+import { queryKeys } from "@/shared/apis/query-keys";
+import { HTTP_METHOD, request } from "@/shared/apis/request";
+
+export const useRecommendedMeal = () => {
+	return useSuspenseQuery({
+		queryKey: queryKeys.recommendedMeal.latest(),
+		queryFn: getRecommendedMeal,
+	});
+};
+
+export const getRecommendedMeal = () => {
+	return request<RecommendedMealView>({
+		method: HTTP_METHOD.GET,
+		url: API_ENDPOINTS.recommendedMeal.recommended,
+	});
+};
