fcd5ee050f3f984a4f1c6c18fef88ef7698e505a
jstar_00 <wltjd6300@naver.com>
2026-01-22 14:33:01 +0900

    chore: 미사용 변수 제거

--- src/pages/my-page/apis/mutations/use-logout.ts
+++ src/pages/my-page/apis/mutations/use-logout.ts
@@ -1,6 +1,5 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { API_ENDPOINTS } from "@/shared/apis/api-endpoints";
-import type { SuccessResponseVoid } from "@/shared/apis/generated/data-contracts";
 import { HTTP_METHOD, request } from "@/shared/apis/request";
 
 export const useLogout = () => {
