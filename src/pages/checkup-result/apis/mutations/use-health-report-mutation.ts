55baf369ada4bb31259cf822caf9c78ddb8156df
mimizae <jim400934@gmail.com>
2026-01-23 17:40:13 +0900

    refactor: 검진 결과 기록이 있는 날짜에 기록 추가 시 에러 핸들링 (409)

--- src/pages/checkup-result/apis/mutations/use-health-report-mutation.ts
+++ src/pages/checkup-result/apis/mutations/use-health-report-mutation.ts
@@ -1,4 +1,5 @@
 import { useMutation } from "@tanstack/react-query";
+import { AxiosError } from "axios";
 import { useNavigate } from "react-router";
 import { ROUTE_PATH } from "@/app/routes/paths";
 import { API_ENDPOINTS } from "@/shared/apis/api-endpoints";
@@ -14,12 +15,19 @@ export const useHealthReportMutation = () => {
 
 	return useMutation({
 		mutationFn: (data: CreateHealthReportRequest) => postHealthReport(data),
+		throwOnError: false,
 		onSuccess: () => {
 			notifySuccess("검진 결과가 추가되었습니다");
 			void navigate(ROUTE_PATH.HOME, { replace: true });
 		},
-		onError: () => {
-			notifyError("검진 결과 저장에 실패했어요.");
+		onError: (error) => {
+			if (error instanceof AxiosError) {
+				if (error.response?.status === 409) {
+					notifyError("해당 검진일자에 결과가 이미 등록되어있습니다");
+					return;
+				}
+			}
+			notifyError("검진 결과 저장에 실패했어요");
 		},
 	});
 };
