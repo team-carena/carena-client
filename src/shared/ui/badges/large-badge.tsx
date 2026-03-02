4dd0c27d63f5d119138179bdd46aac94cbabee6a
mimizae <jim400934@gmail.com>
2026-03-02 13:04:49 +0900

    refactor: 뱃지 스타일 수정

--- src/shared/ui/badges/large-badge.tsx
+++ src/shared/ui/badges/large-badge.tsx
@@ -2,13 +2,13 @@ import { cn } from "@shared/libs/cn";
 import { cva, type VariantProps } from "class-variance-authority";
 
 const largeBadgeVariants = cva(
-	"label01-sb-14 inline-flex w-fit items-center justify-center rounded-[8px] border p-[1.2rem] text-gray-900",
+	"label01-sb-14 inline-flex w-fit items-center justify-center rounded-[8px] border px-[1.2rem] py-[0.8rem]",
 	{
 		variants: {
 			variant: {
-				normal: "border-green-500 bg-green-100",
-				borderline: "border-blue-500 bg-blue-100",
-				suspicious: "border-red-500 bg-red-100",
+				normal: "border-green-500 bg-green-100 text-green-500",
+				borderline: "border-orange-500 bg-orange-100 text-orange-500",
+				suspicious: "border-red-500 bg-red-100 text-red-500",
 			},
 		},
 		defaultVariants: {
