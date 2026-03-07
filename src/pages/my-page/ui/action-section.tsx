f24ddcd1d99cec34ad7db8cf3ef50df6e4703580
mimizae <jim400934@gmail.com>
2026-03-08 01:36:39 +0900

    refactor: 설정 페이지에 회원탈퇴 및 약관 정책 ui 추가

--- src/pages/my-page/ui/action-section.tsx
+++ src/pages/my-page/ui/action-section.tsx
@@ -1,19 +1,30 @@
 import { SelectList } from "@/shared/ui/navigations/select-list";
 
-interface ActionSectionProps {
-	title: string;
+interface ActionItem {
 	label: string;
 	onClick: () => void;
 }
-export const ActionSection = ({
-	title,
-	label,
-	onClick,
-}: ActionSectionProps) => {
+
+interface ActionSectionProps {
+	title: string;
+	items: ActionItem[];
+}
+
+export const ActionSection = ({ title, items }: ActionSectionProps) => {
 	return (
 		<section className="flex flex-col gap-[2rem] rounded-[12px] bg-gray-50 p-[2rem]">
-			<span className="head03-sb-16">{title}</span>
-			<SelectList label={label} onClick={onClick} />
+			<span className="head02-b-16">{title}</span>
+
+			<div className="flex flex-col gap-[2rem]">
+				{items.map((item) => (
+					<SelectList
+						key={item.label}
+						label={item.label}
+						onClick={item.onClick}
+						link={title === "약관 및 정책"}
+					/>
+				))}
+			</div>
 		</section>
 	);
 };
